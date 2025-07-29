7379d204bf3ec44f394e42b0b29e734b73350617
RenyaGit <wadarenya1221@gmail.com>
2025-07-29 18:50:40 +0900

    feat(main): get port using flag package

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"hello-clean-architecture/internal/dependency"
 	"log/slog"
 	"net/http"
 	"os"
+)
+
+const (
+	DefaultPort = ":8080"
 )
 
 func main() {
@@ -16,11 +21,17 @@ func main() {
 	}
 }
 
+func getPort() string {
+	port := flag.String("port", DefaultPort, "Server port (e.g., :8080)")
+	flag.Parse()
+	return *port
+}
+
 func run() error {
 	dependencyInjection := dependency.NewInjection()
 	router := dependencyInjection.NewRouter()
 
-	port := ":8080"
+	port := getPort()
 	slog.Info(fmt.Sprintf("Server starting on port %s", port))
 
 	if err := http.ListenAndServe(port, router); err != nil {
